bf76fa870ca65641e0f81705af1dbb72d1bc3410
Sergey Opasen <ykcyc13@gmail.com>
2023-11-27 21:47:02 +0100

    hw-04

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,14 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef, ReactElement } from "react";
 
-// Опишіть Props
-export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+interface Props {
+  children: ReactNode;
+  onContentEndVisible: () => void;
+}
+
+interface IntersectionObserverOptions {
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number | number[];
+}
+
+export function Observer({
+  children,
+  onContentEndVisible,
+}: Props): ReactElement {
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
+    const options: IntersectionObserverOptions = {
+      rootMargin: "0px",
       threshold: 1.0,
       root: null,
     };
